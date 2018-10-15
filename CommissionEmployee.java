08a2e0cd208711ffa39c11dfb36b1b10d958023b
Stephen wachira <32011924+steve-ryan@users.noreply.github.com>
2018-10-15 17:13:10 +0300

    Add files via upload
    version1

--- CommissionEmployee.java
+++ CommissionEmployee.java
@@ -1,9 +1,21 @@
-public class CommisionEmployee extends Object{
-	private String firstName,
-	private String lastName,
-	private String socialSecurityNumber,
-	private double grossSales,
+public class CommissionEmployee extends Object{
+
+
+	private String firstName;
+	private String lastName;
+	private String socialSecurityNumber;
+	private double grossSales;
 	private double commissionRate;
+
+	public CommissionEmployee(String first, String last, String ssn, double sales, double rate){
+		//implicit call to supperclass Commission
+		firstName= first;
+		lastName = last;
+		socialSecurityNumber = ssn;
+		grossSales = sales;
+		commissionRate = rate;
+		
+	}
         
        public void setFirstName (String first){
 		firstName= first;
@@ -18,33 +30,28 @@ public class CommisionEmployee extends Object{
 		return lastName;
 }
 	public void setSocialSecurityNumber(String ssn){
-		socialSecurityNumber = snn;
+		socialSecurityNumber = ssn;
 }
 	public String getsocialsecurityNumber(){
 		return socialSecurityNumber;
 }
-	public void set GrossSales(double sale){
+	public void setGrossSales(double sales){
 		grossSales = sales;
 }
-	public Double getGrossSales(){
+	public double getGrossSales(){
 		return grossSales;
 }
     public void setCommissionRate(double rate){
 		commissionRate = rate;
 }
-	public Double getCommissionRate(){
+	public double getCommissionRate(){
 		return commissionRate;
 }
 	public double earnings(){
 		return commissionRate * grossSales;
 }
-	public CommissionEmployee(String first, String last, String ssn,double Sales, double rate){
-		//implicit call to supperclass Commission
-		firstName= first;
-		lastName = last;
-		socialSecurityNumber = snn;
-		grossSales = sales;
-		commissionRate = rate;
-		
-}
+	
+
+
+
 }
